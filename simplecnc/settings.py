6f94001ee40791cb9da951e19cba19ea0bfec3f5
coding-n-coding <65457619+coding-n-coding@users.noreply.github.com>
2025-04-04 02:35:18 +0300

    Update settings.py

--- simplecnc/settings.py
+++ simplecnc/settings.py
@@ -128,6 +128,8 @@ MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 
 MIDDLEWARE.insert(1, "whitenoise.middleware.WhiteNoiseMiddleware")
 
+CSRF_TRUSTED_ORIGINS = ["https://simplecnc-1.onrender.com"]
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
