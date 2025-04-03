b3a6a3ea92f96c079fe625554acced078318554d
coding-n-coding <65457619+coding-n-coding@users.noreply.github.com>
2025-04-04 02:47:36 +0300

    Update settings.py

--- simplecnc/settings.py
+++ simplecnc/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-9@q7_%(pkh9($%qvpgd)^k8i)77e5pqdb)$j0(!lqr_t&g(4)*
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["https://simplecnc-1.onrender.com", "localhost", "127.0.0.1"]
+ALLOWED_HOSTS = ["simplecnc-1.onrender.com", "localhost", "127.0.0.1"]
 
 
 # Application definitionsimplecnc.onrender.com
