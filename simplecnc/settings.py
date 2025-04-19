a54aa76a2182e4d26c7ad7f58ac7f51abbcd1239
coding-n-coding <65457619+coding-n-coding@users.noreply.github.com>
2025-04-19 18:23:27 +0300

    Update settings.py

--- simplecnc/settings.py
+++ simplecnc/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-9@q7_%(pkh9($%qvpgd)^k8i)77e5pqdb)$j0(!lqr_t&g(4)*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ["https://simplecnc-1.onrender.com", "localhost", "127.0.0.1"]
 
